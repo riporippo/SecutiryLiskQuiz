168d04236838ff7fccd868c64aad77471ec61f74
tankobualpha <zitofi@mail4.uk>
2025-03-21 23:52:30 +0900

    仮組

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__, static_folder='./templates/images')
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/result', methods=['POST'])
+def result():
+    user_select_answer = int(request.form.get('answer'))
+    correct_answer = 1
+
+    is_correct = (user_select_answer == correct_answer)
+
+    return render_template('result.html', is_correct=is_correct)
 
 @app.route('/mondai-1')
 def question_1():
